c75820b02ecb20e44cd2899fc7d5a6986a26eb41
Anton Gigolaev <giga.a.n.t@mail.ru>
2021-10-10 21:25:41 +0300

    Добавил контроллер уровня на сцену и запустил тестовый уровень
    Продолжил дописывать Level controller
    Теперь яблоко появляется сверху экрана и минус 2 юнита, а препятствие  в нулевой координате

--- Assets/BowGame/Scripts/LevelController.cs
+++ Assets/BowGame/Scripts/LevelController.cs
@@ -6,18 +6,21 @@ public class LevelController : MonoBehaviour
 {
     public GameObject[] ObstaclePrefabs, ApplePrefabs;
     [SerializeField] private GameObject currentObstaclePrefab, currentApplePrefab;
-    [SerializeField] private Vector2 initObstaclePos, initApplePos;
+    [SerializeField] private Vector2 initApplePos;
     [SerializeField] private AppleController currentAppleController;
-    void Start()
+    private void Start()
     {
-        
+        initApplePos = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width/2, Screen.height)) - new Vector3(0, 2, 0);
+        SwitchLevel();
+
     }
     private void SwitchLevel()
     {
         Destroy(currentObstaclePrefab);
         Destroy(currentApplePrefab);
-        currentObstaclePrefab = Instantiate(ObstaclePrefabs[Random.Range(0, ObstaclePrefabs.Length)], initObstaclePos, Quaternion.identity, null);
+        currentObstaclePrefab = Instantiate(ObstaclePrefabs[Random.Range(0, ObstaclePrefabs.Length)], Vector2.zero, Quaternion.identity, null);
         currentApplePrefab = Instantiate(ApplePrefabs[Random.Range(0, ApplePrefabs.Length)], initApplePos, Quaternion.identity, null);
+        currentAppleController = currentApplePrefab.GetComponent<AppleController>();
         ///TODO: event invoke from apple and subscription in this script
     }
     
